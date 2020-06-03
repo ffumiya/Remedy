0b0df83aae564c08db08d4ec1f35f18190066ae1
Masayoshi Tanaka <masayoshi108justin@gmail.com>
2020-06-03 19:00:32 +0900

    bootstrapの削除

--- Remedy-Client/nuxt.config.js
+++ Remedy-Client/nuxt.config.js
@@ -26,11 +26,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [
-    "@/assets/css/bootstrap.scss",
-    "@/assets/css/bootstrap-gtid.scss",
-    "@/assets/css/bootstrap-reboot.scss"
-  ],
+  css: [],
   /*
    ** Plugins to load before mounting the App
    */
