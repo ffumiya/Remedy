62b824f4afbb0c28d8e8f73a872c4c6d56f41293
Masayoshi Tanaka <masayoshi108justin@gmail.com>
2020-08-15 22:52:39 +0900

    SSL可対応メソッド名修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (request()->isSecure()) {
-            \URL::forceSceme('https');
+            \URL::forceScheme('https');
         }
     }
 }
