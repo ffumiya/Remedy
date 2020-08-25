09a5332c63c0bbae140f5b1ab9c2d45830d6c377
Masayoshi Tanaka <masayoshi108justin@gmail.com>
2020-08-25 16:30:30 +0900

    ユーザーの新規登録時パスワードをハッシュ化

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
         $user = User::create([
             User::NAME => $request->name,
             User::EMAIL => $request->email,
-            User::PASSWORD => $request->password,
+            User::PASSWORD => Hash::make($request->password),
             User::CLINIC_ID => \Auth::user()->clinic_id,
             User::API_TOKEN => str_random(80),
         ]);
