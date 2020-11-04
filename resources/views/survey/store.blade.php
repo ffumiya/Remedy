6f25defef8ec40a45d1ed322b1250a5fd6321e16
m-tanaka <masayoshi@BengalPro.local>
2020-11-04 21:42:10 +0900

    アンケート投稿後画面を作成

--- resources/views/survey/store.blade.php
+++ resources/views/survey/store.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="h1">Stored</div>
+<div class="container">
+    <div class="text-center">
+        <div>
+            <p class="h3">
+                アンケートへのご協力ありがとうございました。
+            </p>
+            <p>
+                ページを閉じてください。
+            </p>
+        </div>
+    </div>
+</div>
 @endsection
 
 @include('footer')
