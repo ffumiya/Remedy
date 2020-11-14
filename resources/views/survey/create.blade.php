d4f089774de8c1a1d4153a0d517162f34020cc61
m-tanaka <masayoshi@BengalPro.local>
2020-11-14 11:12:41 +0900

    アンケート画面に満足度数値の現在選択値を表示するように改修

--- resources/views/survey/create.blade.php
+++ resources/views/survey/create.blade.php
@@ -19,11 +19,11 @@
                 @csrf
                 <input type="hidden" name="survey_token" value="{{ $survey_token }}">
                 <input type="hidden" name="role" value="{{ $role }}">
-                <div class="form-group">
+                <div class="form-group mt-5 mb-5">
                     <label for="name">お名前を入力してください</label>
                     <input type="text" class="form-control" id="name" name="name" placeholder="例：山田花子">
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-5 mb-5">
                     <label for="satisfaction_level">問１：本日の診療はどの程度ご理解できましたか？</label>
                     <div class="row justify-content-between mt-3">
                         <div class="col text-left">
@@ -37,11 +37,18 @@
                     </div>
                     <input type="range" class="custom-range" min="1" max="10" id="satisfaction_level"
                         name="satisfaction_level">
+                    <p>
+                        現在選択中の数字：
+                        <strong id="slider-value">6</strong>
+                    </p>
                 </div>
-                <div class="form-group">
-                    <label for="comment1">問２：追加で質問したいことはありますか？(300文字以内)</label>
-                    <textarea class="form-control" name="comment1" id="comment1" cols="30" rows="10" maxlength="300"
-                        placeholder="質問したいことを入力してください。"></textarea>
+                <div class="form-group mt-5 mb-5">
+                    <label for="comment">問２：質問したいことはありますか？(300文字以内)</label>
+                    <textarea class="form-control" name="comment" id="comment" cols="30" rows="10" maxlength="300"
+                        placeholder="質問したいことを入力してください" onkeydown="showLength(value)"></textarea>
+                    <div class="text-right">
+                        <p id="comment-length">0文字</p>
+                    </div>
                 </div>
                 <div class="text-center">
                     <input type="submit" class="btn btn-primary btn-lg" value="送信">
@@ -53,5 +60,22 @@
 </div>
 
 @endsection
-
 @include('footer')
+
+@section('script')
+<script>
+    function showLength(str) {
+        document.getElementById("comment-length").innerHTML = str.length + "文字";
+    }
+    document.addEventListener( 'DOMContentLoaded' , function( e ) {
+        var elem = document.getElementById('satisfaction_level');
+        var target = document.getElementById('slider-value');
+        var rangeValue = (elem, target) => {
+            return function(evt){
+            target.innerHTML = elem.value;
+            }
+        }
+        elem.addEventListener('input', rangeValue(elem, target));
+    });
+</script>
+@endsection
