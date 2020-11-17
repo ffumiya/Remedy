b81e897a82ffae7f5caaa79cc8ea19f41bd62fcb
m-tanaka <masayoshi@BengalPro.local>
2020-11-17 20:05:15 +0900

    ZOOMによるメール内容の変更

--- app/Mail/ZoomNewCreationNotification.php
+++ app/Mail/ZoomNewCreationNotification.php
@@ -2,9 +2,12 @@
 
 namespace App\Mail;
 
+use App\Models\Clinic;
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
 
 class ZoomNewCreationNotification extends ZoomNotification
 {
@@ -17,13 +20,23 @@ class ZoomNewCreationNotification extends ZoomNotification
      */
     public function build()
     {
+        $patient_name = User::find($this->event->guest_id)->name;
+        $clinic_name = Clinic::find(Auth::user()->clinic_id)->name;
+        $doctor_name = User::find($this->event->host_id)->name;
         $start = $this->event[Event::START];
         $end = $this->event[Event::END];
         $remedy_url = $this->getRemedyURL();
         $zoom_url = $this->event[Event::ZOOM_JOIN_URL];
 
         return $this->from($this->fromEmail)
-            ->subject('Remedy事務局よりお知らせ')
-            ->view('mail.new_creation', compact(['start', 'end', 'remedy_url', 'zoom_url']));
+            ->subject("${clinic_name}/次回の診察日程について　Remedyご案内事務局")
+            ->view('mail.new_creation', compact([
+                'patient_name',
+                'clinic_name',
+                'doctor_name',
+                'start', 'end',
+                'remedy_url',
+                'zoom_url'
+            ]));
     }
 }
--- resources/views/mail/delete_schedule.blade.php
+++ resources/views/mail/delete_schedule.blade.php
@@ -5,25 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Remedy事務局よりお知らせ</title>
+    <title>{{ $clinic_name }}/次回の診察日程について　Remedyご案内事務局</title>
 </head>
 
 <body>
-    <h2>○○病院よりオンライン診療がキャンセルされました</h2>
+    <div>
+        <p>{{ $patient_name }} 様</p>
+        <p>オンライン診療サービス「Remedy」のご案内事務局です。</p>
+        <p>{{ $clinic_name }} 様での下記の診療予定が中止されました。</p>
+    </div>
 
-    <table>
-        <tr>
-            <th>日にち</th>
-            <td>{{ $start->format('Y年m月d日') }}</td>
-        </tr>
-        <tr>
-            <th>時間</th>
-            <td>{{ $start->format('H:i') }}～{{ $end->format('H:i') }}</td>
-        </tr>
-    </table>
+    <hr>
+
+    <div>
+        <p>【病院名】{{ $patient_name }}</p>
+        <p>【担当医】{{ $doctor_name }}</p>
+        <p>【日　時】{{ $start->format('Y年m月d日 H時:i分') }}開始</p>
+    </div>
+
+    <hr>
+
     <p>
-        診療が必要となった際は改めて担当医へご連絡ください。
+        ※本メールは送信専用となっておりますため、ご連絡頂いてもご返信が出来かねますので予めご了承ください。
+        なお、日程変更・ご不明な点等がございましたらご利用の病院までお問合せください。
     </p>
+
+    <hr>
+
+    <p>病状説明専門のオンライン診療サービス「Remedy」</p>
+    <p>ご案内事務局</p>
+    <hr>
 </body>
 
 </html>
